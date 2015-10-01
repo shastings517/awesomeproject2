3487093bcf938beb1275bcb5791ae3782b80bcfd
Samuel Hastings <shastings517@gmail.com>
2015-09-30 17:56:46 -0700

    cleaned up unnecessary code

--- middleware/routeHelper.js
+++ middleware/routeHelper.js
@@ -22,18 +22,6 @@ var routeHelpers = {
     });
   },
 
-  // ensureCorrectUserForComment: function(req, res, next) {
-  //   db.Comment.findById(req.params.id).populate('author').exec(function(err,comment){
-  //     console.log(comment);
-  //     if (comment.author !== undefined && comment.author.id != req.session.id) {
-  //       res.redirect('/posts/'+ comment.post +'/comments');
-  //     }
-  //     else {
-  //      return next();
-  //     }
-  //   });
-  // },
-
   preventLoginSignup: function(req, res, next) {
     if (req.session.id !== null && req.session.id !== undefined) {
       console.log(req.session.id);
