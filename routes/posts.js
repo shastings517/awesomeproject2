7be27efacb88e643846a2a8f9d6ed916fa4991c4
Samuel Hastings <shastings517@gmail.com>
2015-10-01 15:38:52 -0700

    working on edit page and put

--- routes/posts.js
+++ routes/posts.js
@@ -89,7 +89,7 @@ router.post('/', function(req,res) {
                   }
                   console.log(post.score);
                   if(post.improvements === "" || post.improvements === "none" || post.improvements === "nothing" || post.improvements === "no"){
-                    post.score -= 5;
+                    post.score -= 10;
                   }
 
                   post.score += (post.lowSentiment + post.highSentiment) * 9;
@@ -105,7 +105,7 @@ router.post('/', function(req,res) {
             console.log("uhhh we fucked up...", error, response);
           }
         });
-    });
+      });
     }
   });
 });
@@ -130,13 +130,81 @@ router.get('/:id/edit', function(req,res){
 
 //UPDATE POST
 router.put('/:id', function(req,res){
+  console.log("THIS IS WHATS COMING IN ", req.body)
   var show_page = "/posts/" + req.params.id;
   db.Post.findByIdAndUpdate(req.params.id, req.body.post, function(err,post){
     if (err) {
       console.log(err);
       res.render('posts/edit');
     } else {
-    res.redirect(show_page);
+      db.User.findById(req.session.id, function(err, user) {
+        request("http://gateway-a.watsonplatform.net/calls/text/TextGetTextSentiment?apikey=" + process.env.ALCHEMY_API_KEY + "&outputMode=json&text=" + encodeURIComponent(req.body.post.high),
+          function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+              console.log(JSON.parse(body).docSentiment.score);
+              post.highSentiment = JSON.parse(body).docSentiment.score;
+
+              request("http://gateway-a.watsonplatform.net/calls/text/TextGetTextSentiment?apikey=" + process.env.ALCHEMY_API_KEY + "&outputMode=json&text=" + encodeURIComponent(req.body.post.low),
+              function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                  post.score = 100;
+                  console.log(JSON.parse(body).docSentiment.score);
+                  post.lowSentiment = JSON.parse(body).docSentiment.score;
+                  console.log(post.date);
+                  user.posts.push(post);
+                  console.log(post);
+                  post.user = user._id;
+                  //assign point values to post schema variables
+                  if(post.sleep >= 8){
+                    post.score -= 10;
+                  }
+                  else if(post.sleep >= 5){
+                    post.score -= 15;
+                  }
+                  else if(post.sleep >= 1){
+                    post.score -= 20;
+                  }
+                  else if(post.sleep === 0){
+                    post.score -= 25;
+                  }
+                  console.log(post.score);
+                  if(post.meditate === "no"){
+                    post.score -= 15;
+                  }
+                  console.log(post.score);
+                  if(post.diet === "poor"){
+                    post.score -= 25;
+                  }
+                  else if(post.diet === "ok"){
+                    post.score -= 20;
+                  }
+                  else if(post.diet === "fair"){
+                    post.score -= 15;
+                  }
+                  else if(post.diet === "good"){
+                    post.score -= 10;
+                  }
+                  console.log(post.score);
+                  if(post.improvements === "" || post.improvements === "none" || post.improvements === "nothing" || post.improvements === "no"){
+                    post.score -= 10;
+                  }
+
+                  post.score += (post.lowSentiment + post.highSentiment) * 9;
+                  post.score = (post.score).toFixed(2);
+                  
+                  post.save();
+                  user.save();
+                  res.redirect(show_page);
+                  // res.redirect("/posts");
+                }
+            });
+          }
+          else {
+            console.log("uhhh we fucked up...", error, response);
+          }
+        });
+      });
+      
     }
   });
 });
